e81540ffeb5f51adae2c1f59d395f19635a9314e
Koji255 <koji25ony1@gmail.com>
2024-10-26 09:21:50 +0300

    mysql connection

--- project/settings.py
+++ project/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_db',
+        'USER': 'koji',
+        'PASSWORD': 'kojikoji',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
