7bc26e68c49fbd68fd4ad38a52e3f52ddc7da724
Koji255 <koji25ony1@gmail.com>
2024-10-27 09:11:48 +0300

    1st prototype of user model & db.sqlite3 deleted

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,18 @@
+import datetime
+
 from django.db import models
+from django.db.models import Q
 
 # Create your models here.
 class User(models.Model):
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50, default="")
+    nickname = models.CharField(max_length=50, default="")
+    email = models.EmailField(default=None)
+    password = models.CharField(max_length=20)
+    account_creation_date = models.DateTimeField(default=datetime.datetime.today())
+
+    class Meta:
+
+        db_table = "users"
+        ordering = ["account_creation_date"]
