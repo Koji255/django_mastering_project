4c058325f46c1276b19e8da6d50012ab9dd8f147
Koji255 <koji25ony1@gmail.com>
2024-10-27 09:35:41 +0300

    register admin for 'Users' table & override __str__

--- myapp/models.py
+++ myapp/models.py
@@ -1,18 +1,20 @@
-import datetime
+from django.utils import timezone
 
 from django.db import models
 from django.db.models import Q
 
 # Create your models here.
 class User(models.Model):
-    surname = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50, default="")
     lastname = models.CharField(max_length=50, default="")
     nickname = models.CharField(max_length=50, default="")
     email = models.EmailField(default=None)
-    password = models.CharField(max_length=20)
-    account_creation_date = models.DateTimeField(default=datetime.datetime.today())
+    password = models.CharField(max_length=20, default="")
+    account_creation_date = models.DateTimeField(default=timezone.now())
 
     class Meta:
-
         db_table = "users"
         ordering = ["account_creation_date"]
+
+    def __str__(self):
+        return self.nickname
